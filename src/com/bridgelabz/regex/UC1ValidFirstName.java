c48a0743e7ffca0b427db672d437b3ac66edc4a9
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-02-10 01:06:37 +0530

    Added : Valid First Name

--- src/com/bridgelabz/regex/UC1ValidFirstName.java
+++ src/com/bridgelabz/regex/UC1ValidFirstName.java
@@ -1,2 +1,21 @@
-package com.bridgelabz.regex;public class UC1ValidFirstName {
+package com.bridgelabz.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class UC1ValidFirstName {
+    static Pattern pattern = Pattern.compile("^([A-Z]?[a-z]{3,})$");
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        // Method To Check First Name With Regex Start With Cap And 3 Min. Character
+        System.out.println("Enter the first Name: ");
+        String firstName = scan.nextLine();
+
+        if(Pattern.matches("^[A-Z][a-z]{2,}", firstName))
+            System.out.println("First name is valid");
+        else
+            System.out.println("Entered first name is invalid");
+    }
 }
--- src/com/bridgelabz/regex/UC2ValidLastName.java
+++ src/com/bridgelabz/regex/UC2ValidLastName.java
@@ -1,2 +1,28 @@
-package com.bridgelabz.regex;public class UC2ValidLastName {
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UC2ValidLastName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // Method To Check First Name With Regex Start With Cap And 3 Min. Character
+        System.out.println("Enter the first Name: ");
+        String firstName = scan.nextLine();
+        System.out.println("Enter the last Name :");
+        String lastName = scan.nextLine();
+
+        if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
+            System.out.println("First name is valid");
+        } else {
+            System.out.println("Entered first name is invalid");
+        }
+        scan.close();
+        // Method To Check Last Name With Regex Start With Cap And 3 Min. Character
+        if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
+            System.out.println("Last name is valid");
+        } else {
+            System.out.println("Entered last name is invalid");
+        }
+    }
 }
